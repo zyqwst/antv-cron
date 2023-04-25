1af4cc0f9de0e2cad3125568ec2076012efd46dc
albert <790321193@qq.com>
2023-04-25 17:36:32 +0800

    feat:Initial

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: './',
+  productionSourceMap:false,
   transpileDependencies: true,
   pages: {
     index: {
